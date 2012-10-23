3da217c8e22cf0f032f156617a8e9b4fbf90a9a5
Jasper <jaspershemilt@virginmedia.co.uk>
2012-10-23 14:24:04 +0100

    Nothing

---
+++ OpenCViPhoneApplication1/ColorSpaceMaker.cpp
@@ -0,0 +1,14 @@
+//
+//  ColorSpaceMaker.cpp
+//  OpenCViPhoneApplication1
+//
+//  Created by Jasper Shemilt on 22/10/2012.
+//  Copyright (c) 2012 University of Houston - Main Campus. All rights reserved.
+//
+
+#include "ColorSpaceMaker.h"
+
+#include <opencv2/core/mat.hpp>
+
+
+
---
+++ OpenCViPhoneApplication1/ColorSpaceMaker.h
@@ -0,0 +1,14 @@
+//
+//  ColorSpaceMaker.h
+//  OpenCViPhoneApplication1
+//
+//  Created by Jasper Shemilt on 22/10/2012.
+//  Copyright (c) 2012 University of Houston - Main Campus. All rights reserved.
+//
+
+#ifndef __OpenCViPhoneApplication1__ColorSpaceMaker__
+#define __OpenCViPhoneApplication1__ColorSpaceMaker__
+
+#include <iostream>
+
+#endif /* defined(__OpenCViPhoneApplication1__ColorSpaceMaker__) */
