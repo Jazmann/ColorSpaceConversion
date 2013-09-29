7cd41fcc6b8d0fc103f2ae3f21c4fd6e65b526f2
JasperShemilt <jjshemilt@toscali.co.uk>
2013-09-29 10:15:40 +0100

    Matlab Added generation of Transform Matrix

--- matLab/ColorSpaceStats/run.m
+++ matLab/ColorSpaceStats/run.m
@@ -1,18 +1,10 @@
-
-angle = -0.8728;
-[Y B R bin A] = colorStats( 0.114, 0.299, 0, 0, 255, 256, 0, 255, 256, 0, 255, 256);
-Z = squeeze(sum(bin,2));
-x(1,:) = GaussianFit( B, R, squeeze(sum(bin,2)), [1,A(3),20.5,A(2),20.5,-pi/8], 'spline', 0);
-angle = angle + x(1,6);
-[Y B R bin A] = colorStats( 0.114, 0.299, angle, 0, 255, 256, 0, 255, 256, 0, 255, 256);
-x(2,:) = GaussianFit( B, R, squeeze(sum(bin,2)), [1,A(3),20.5,A(2),20.5,0], 'spline', 0);
-angle = angle + x(2,6);
-[Y B R bin A] = colorStats( 0.114, 0.299, angle, 0, 255, 256, 0, 255, 256, 0, 255, 256);
-x(3,:) = GaussianFit( B, R, squeeze(sum(bin,2)), [1,A(3),20.5,A(2),20.5,0], 'spline', 0);
-angle = angle + x(3,6);
-[Y B R bin A] = colorStats( 0.114, 0.299, angle, 0, 255, 256, 0, 255, 256, 0, 255, 256);
-x(4,:) = GaussianFit( B, R, squeeze(sum(bin,2)), [1,A(3),20.5,A(2),20.5,0], 'spline', 0);
-angle = angle + x(4,6);
-[Y B R bin A] = colorStats( 0.114, 0.299, angle, 0, 255, 256, 0, 255, 256, 0, 255, 256);
-x(5,:) = GaussianFit( B, R, squeeze(sum(bin,2)), [1,A(3),20.5,A(2),20.5,0], 'spline', 0);
-angle = angle + x(5,6);
+n=4;
+angle = zeros(n+1);
+x = zeros(n,6);
+angle(1) = -0.9313;
+for i=1:n
+    [Y, B, R, bin, A] = colorStats( 0.114, 0.299, angle(i), 0, 255, 256, 0, 255, 256, 0, 255, 256);
+    x(i,:) = GaussianFit( B, R, squeeze(sum(bin,2)), [1,A(3),20.5,A(2),20.5,-pi/8], 'spline', 0);
+    angle(i+1) = angle(i) + x(1,6);
+end
+ T =  int8(transformationMatrix(0.114, 0.299, angle(n+1),255,255,255))
