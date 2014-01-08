02c53155536941db6096b80af09896772275d16a
Nayef <gmbetax@gmail.com>
2014-01-08 03:27:06 +0000

    Very strange developments.

---
+++ matLab/ColorSpaceStats/imgCStats.m
@@ -0,0 +1,5 @@
+
+[YOut2, BOut2, ROut2, binOut2, AOut2] = imgColorStats(imgRotP1(257:294,130:173,:),0,255,256,0,255,256,0,255,256,minChan,maxChan);
+
+C2(1,:) = AOut2(1,:);
+ 
--- matLab/ColorSpaceStats/runExampleThumb.m
+++ matLab/ColorSpaceStats/runExampleThumb.m
@@ -1,4 +1,4 @@
-skin = colorSpace(theta, C_Jasper(1,:), sig .* Sigma(1,:), [3,3,3], 0, 255, 0, 255, 1, 0);
+skinJasper = colorSpace(theta, C_Jasper(1,:), Sigma(1,:), [3,3,3], 0, 255, 0, 255, 1, 0);
 
 img5 = imread('thumb1.JPG');
 img6 = imread('thumb2.JPG');
@@ -8,28 +8,28 @@ img9 = imread('thumb5.JPG');
 img10 = imread('thumb6.JPG');
 img11 = imread('thumb7.JPG');
 
-imgRot5 = skin.toRot(img5);
-imgRot6 = skin.toRot(img6);
-imgRot7 = skin.toRot(img7);
-imgRot8 = skin.toRot(img8);
-imgRot9 = skin.toRot(img9);
-imgRot10 = skin.toRot(img10);
-imgRot11 = skin.toRot(img11);
+imgRot5 = skinJasper.toRot(img5);
+imgRot6 = skinJasper.toRot(img6);
+imgRot7 = skinJasper.toRot(img7);
+imgRot8 = skinJasper.toRot(img8);
+imgRot9 = skinJasper.toRot(img9);
+imgRot10 = skinJasper.toRot(img10);
+imgRot11 = skinJasper.toRot(img11);
 
-imgRotScaled5 = skin.toRotScaled(img5);
-imgRotScaled6 = skin.toRotScaled(img6);
-imgRotScaled7 = skin.toRotScaled(img7);
-imgRotScaled8 = skin.toRotScaled(img8);
-imgRotScaled9 = skin.toRotScaled(img9);
-imgRotScaled10 = skin.toRotScaled(img10);
-imgRotScaled11 = skin.toRotScaled(img11);
+imgRotScaled5 = skinJasper.toRotScaled(img5);
+imgRotScaled6 = skinJasper.toRotScaled(img6);
+imgRotScaled7 = skinJasper.toRotScaled(img7);
+imgRotScaled8 = skinJasper.toRotScaled(img8);
+imgRotScaled9 = skinJasper.toRotScaled(img9);
+imgRotScaled10 = skinJasper.toRotScaled(img10);
+imgRotScaled11 = skinJasper.toRotScaled(img11);
 
-imgRotCompactScaled5 = skin.toRotCompactScaled(img5);
-imgRotCompactScaled6 = skin.toRotCompactScaled(img6);
-imgRotCompactScaled7 = skin.toRotCompactScaled(img7);
-imgRotCompactScaled8 = skin.toRotCompactScaled(img8);
-imgRotCompactScaled9 = skin.toRotCompactScaled(img9);
-imgRotCompactScaled10 = skin.toRotCompactScaled(img10);
-imgRotCompactScaled11 = skin.toRotCompactScaled(img11);
+imgRotCompactScaled5 = skinJasper.toRotCompactScaled(img5);
+imgRotCompactScaled6 = skinJasper.toRotCompactScaled(img6);
+imgRotCompactScaled7 = skinJasper.toRotCompactScaled(img7);
+imgRotCompactScaled8 = skinJasper.toRotCompactScaled(img8);
+imgRotCompactScaled9 = skinJasper.toRotCompactScaled(img9);
+imgRotCompactScaled10 = skinJasper.toRotCompactScaled(img10);
+imgRotCompactScaled11 = skinJasper.toRotCompactScaled(img11);
 
 
