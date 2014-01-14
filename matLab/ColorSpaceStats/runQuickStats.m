333a43caf6611df8b690c44c14fd36efec182381
Jasper <jaspershemilt@virginmedia.co.uk>
2014-01-14 19:57:37 +0000

    MatLAB

--- matLab/ColorSpaceStats/runQuickStats.m
+++ matLab/ColorSpaceStats/runQuickStats.m
@@ -18,15 +18,15 @@ Cbv = 0:1/(CbBins-1):1;
 
 trans = transform(ang(1),'yes');
 
-AOut = trans.toRot((AOutRGB ./ 255));
+c(1,:) = trans.toRot((AOutRGB ./ 255));
 
 for i=1:nMax
  chromBinOut=binRot2D(chromBin,ang(i));
  %   x = [Amp,x0,wx,y0,wy,fi]
- x(i,:) = GaussianFit( Cbv, Crv, chromBinOut, [1,AOut(3),sigma(i,3),AOut(2),sigma(i,2),0], 'spline', 0);
+ x(i,:) = GaussianFit( Cbv, Crv, chromBinOut, [1,c(i,3),sigma(i,3),c(i,2),sigma(i,2),0], 'spline', 0);
  ang(i+1) = ang(i) + x(i,6);
- c(i+1,:) = [128, x(i,4),x(i,2)];
- sigma(i+1,:) = [255/sqrt(2), x(i,5), x(i,3)];
+ c(i+1,:) = [0.5, x(i,4),x(i,2)];
+ sigma(i+1,:) = [1/sqrt(2), x(i,5), x(i,3)];
  if abs(x(i,6)) < tol 
      break
  end
@@ -36,6 +36,7 @@ theta = ang(i+1);
 g = [ 1, 255/(sqrt(2) .* sigma(i+1,2)), 255/(sqrt(2) .* sigma(i+1, 3))];
 sigma = sigma(1:i+1,:);
 c = c(1:i+1,:);
+ang = ang(1:i+1,1);
 
 C = zeros(1,3); C = c(end,:);
 Sigma = sigma(end,:); Sigma = sigma(end,:);
