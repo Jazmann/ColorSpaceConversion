167fd3137cc15cef1890b82daa5733e566d50044
Nayef <gmbetax@gmail.com>
2014-01-15 01:25:11 +0000

    Other things.

--- matLab/ColorSpaceStats/negGet.m
+++ matLab/ColorSpaceStats/negGet.m
@@ -1,9 +1,11 @@
-function [ ] = negGet( img1, img2, p1, p2 )
+function [neg] = negGet( img1, img2, p1, p2 )
 
-dImg1 = im2double(img1);
-dImg2 = im2double(img2);
+%dImg1 = im2double(img1);
+%dImg2 = im2double(img2);
 
-neg = dImg1(p2(2):p1(2),p1(1):p2(1),:) - dImg2(p2(2):p1(2),p1(1):p2(1),:);
+%neg = dImg1(p2(2):p1(2),p1(1):p2(1),:) - dImg2(p2(2):p1(2),p1(1):p2(1),:);
+
+neg = img1(p2(2):p1(2),p1(1):p2(1),:) - img2(p2(2):p1(2),p1(1):p2(1),:);
 
 %imageChannels(neg,cutFig1);
 
@@ -15,21 +17,21 @@ neg(:,:,1) = neg(:,:,1) ./ (max(max(max(neg(:,:,:)))));
 neg(:,:,2) = neg(:,:,2) ./ (max(max(max(neg(:,:,2:3)))));
 neg(:,:,3) = neg(:,:,3) ./ (max(max(max(neg(:,:,2:3)))));
 
-cutFig1 = figure('Name','Cut Fig Min/Max','NumberTitle','off');
-imageChannels(neg,cutFig1);
+%cutFig1 = figure('Name','Cut Fig Min/Max','NumberTitle','off');
+%imageChannels(neg,cutFig1);
 
-edgeFilter = fspecial('gaussian');
-smoothNeg = imfilter(neg,edgeFilter,'same');
+%edgeFilter = fspecial('gaussian');
+%smoothNeg = imfilter(neg,edgeFilter,'same');
 
-cutFig2 = figure('Name','Cut Fig Smooth','NumberTitle','off');
-imageChannels(smoothNeg,cutFig2);
+%cutFig2 = figure('Name','Cut Fig Smooth','NumberTitle','off');
+%imageChannels(smoothNeg,cutFig2);
 
-negPressureEdge = zeros(size(smoothNeg));
-negPressureEdge(:,:,1) = edge(smoothNeg(:,:,1),'canny',0.6);
-negPressureEdge(:,:,2) = edge(smoothNeg(:,:,2),'canny',0.6);
-negPressureEdge(:,:,3) = edge(smoothNeg(:,:,3),'canny',0.6);
+%negPressureEdge = zeros(size(smoothNeg));
+%negPressureEdge(:,:,1) = edge(smoothNeg(:,:,1),'canny',0.6);
+%negPressureEdge(:,:,2) = edge(smoothNeg(:,:,2),'canny',0.6);
+%negPressureEdge(:,:,3) = edge(smoothNeg(:,:,3),'canny',0.6);
 
-cutFig3 = figure('Name','Cut Fig Edge','NumberTitle','off');
-imageChannels(negPressureEdge,cutFig3);
+%cutFig3 = figure('Name','Cut Fig Edge','NumberTitle','off');
+%imageChannels(negPressureEdge,cutFig3);
 
 end
