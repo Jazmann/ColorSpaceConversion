ba1f36062cbe131cd9c22e6c764f603fb57af77f
Jasper <jaspershemilt@virginmedia.co.uk>
2014-01-30 19:34:58 +0000

    MatLab mat files and Bin.m

--- matLab/ColorSpaceStats/Bin.m
+++ matLab/ColorSpaceStats/Bin.m
@@ -343,6 +343,8 @@ classdef Bin
             [ xdata(:,:,1), xdata(:,:,2)] =  meshgrid(obj.vals{2}, obj.vals{1});
             gF = D2GaussFunctionRot([obj.gAmp, obj.gMean(2), obj.gSigma(2), obj.gMean(1),obj.gSigma(1),obj.gTheta],xdata);
             contour(obj.vals{2},obj.vals{1},gF, nContours);
+                xlabel(obj.axisNames(2));
+                ylabel(obj.axisNames(1));
             figure(gcf);
         end
     end
