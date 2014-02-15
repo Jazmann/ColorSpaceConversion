206d662ba42cbe04ecc9d2dadcbe43696e578d23
Jasper <jaspershemilt@virginmedia.co.uk>
2014-02-15 18:23:59 +0000

    MatLab contour detection with box

--- matLab/ColorSpaceStats/findContours.m
+++ matLab/ColorSpaceStats/findContours.m
@@ -2,10 +2,15 @@
 classdef findContours
     
     properties
+        name = 'Contours'; % A discriptive name of the Contours.
+        axisNames = ['x,','y'];
         img;
         contours;
         hierarchy;
         nContours;
+        box;
+        boxVerts;
+        boxEdges;
     end
     
     methods
@@ -15,6 +20,45 @@ classdef findContours
             [obj.contours, obj.hierarchy] = cv.findContours(image);
             obj.nContours=size(obj.contours,2);
         end
+        
+        function obj = findRect(obj)
+            cv.minAreaRect(obj.contours{1})
+            obj.box = cv.minAreaRect(obj.contours{1});
+            
+            
+            obj.boxEdges = [-obj.box.size(1)/2, 0               , obj.box.size(1)/2, 0;
+                0                ,obj.box.size(2)/2, 0                ,-obj.box.size(2)/2];
+            
+            obj.boxVerts = [-obj.box.size(1)/2,obj.box.size(1)/2, obj.box.size(1)/2,-obj.box.size(1)/2;
+                obj.box.size(2)/2,obj.box.size(2)/2,-obj.box.size(2)/2,-obj.box.size(2)/2];
+            
+            rr = [cos(obj.box.angle*2*pi/360.0), - sin(obj.box.angle*2*pi/360.0); sin(obj.box.angle*2*pi/360.0), cos(obj.box.angle*2*pi/360.0)];
+            obj.boxEdges = rr * obj.boxEdges;
+            obj.boxVerts = rr * obj.boxVerts;
+            obj.boxEdges(1,:) = obj.boxEdges(1,:) + obj.box.center(1);
+            obj.boxEdges(2,:) = obj.boxEdges(2,:) + obj.box.center(2);
+            obj.boxVerts(1,:) = obj.boxVerts(1,:) + obj.box.center(1);
+            obj.boxVerts(2,:) = obj.boxVerts(2,:) + obj.box.center(2);
+            
+            
+            line(obj.boxVerts(1,:),obj.boxVerts(2,:))
+            line([obj.boxVerts(1,:),obj.boxVerts(1,1)],[obj.boxVerts(2,:),obj.boxVerts(2,1)])
+            
+        end
+        
+        function showRect(obj)
+            figure('Name',horzcat('Contours for ',obj.name),'NumberTitle','off');
+            imagesc(obj.img)
+            
+            line(obj.boxVerts(1,:),obj.boxVerts(2,:))
+            line([obj.boxVerts(1,:),obj.boxVerts(1,1)],[obj.boxVerts(2,:),obj.boxVerts(2,1)])
+            
+            xlabel(obj.axisNames(2));
+            ylabel(obj.axisNames(1));
+            title(horzcat('Contours for ',obj.name));
+            figure(gcf)
+        end
+        
         
         
         function out = showContours(obj)
@@ -28,6 +72,14 @@ classdef findContours
                 imagesc(out)
                 figure(gcf)
             end
+            
+            figure('Name',horzcat('Contours for ',obj.name),'NumberTitle','off');
+            imagesc(out)
+            xlabel(obj.axisNames(2));
+            ylabel(obj.axisNames(1));
+            title(horzcat('Contours for ',obj.name));
+            figure(gcf)
+            
         end
         
         
