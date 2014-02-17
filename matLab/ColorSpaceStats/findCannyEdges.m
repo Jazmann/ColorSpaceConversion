134a112eca7b5510639d5ffff3b5cf12f8f26f78
Jasper <jaspershemilt@virginmedia.co.uk>
2014-02-17 10:30:29 +0000

    MatLab contour detection with box

--- matLab/ColorSpaceStats/findCannyEdges.m
+++ matLab/ColorSpaceStats/findCannyEdges.m
@@ -5,12 +5,23 @@ if nargin <=2
         tol_start = 95;
     end
 end
-total = uint8(zeros(size(img)));
-result = zeros(size(img));
-for i=0:7
-    tol= i*(tol_end-tol_start)/7 + tol_start;
-    result = cv.Canny(img, [tol,(tol * 0.8)])/255;
-    total = total + result * 2^i;
+if length(size(img))>2
+    total = uint8(zeros(size(img)));
+    
+    for i=1:size(img,3)
+        total(:,:,i) = cv.Canny(img(:,:,i), [tol_end,tol_start]);
+    end
+else
+    
+    total = uint8(zeros(size(img)));
+    result = zeros(size(img));
+    
+    for i=0:7
+        tol= i*(tol_end-tol_start)/7 + tol_start;
+        result = cv.Canny(img, [tol,(tol * 0.8)])/255;
+        total = total + (result .* 2^i);
+    end
+    
 end
 
 end
